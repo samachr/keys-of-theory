f21beb0340b932353cc0396c406a350c2f09bfd9
Samuel Christensen <samachr@gmail.com>
2015-01-23 11:58:57 -0700

    some style cleanup and user experience

--- angular-tiny/app.js
+++ angular-tiny/app.js
@@ -3,7 +3,15 @@ $scope.majorScalePattern = [0,2,4,5,7,9,11];
 $scope.whiteKeyList = [0,2,4,5,7,9,11,12,14,16,17,19,21,23];
 $scope.blackKeyList = [1,3,6,8,10,13,15,18,20,22];
 $scope.keysounds = [];
-$scope.keyloadedpercent = 4;
+
+function isWhite(keyNum) {
+  for (var i=0; i<$scope.whiteKeyList.length; i++) {
+    if (keyNum === $scope.whiteKeyList[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 $scope.getAudio = function () {
   for (var i = 0; i < 24; i++) {
@@ -17,5 +25,19 @@ $scope.play = function(key) {
   console.log("playing key" + key);
   $scope.keysounds[key].currentTime = 0;
   $scope.keysounds[key].play();
+  document.getElementById("key"+key).style.background = "#2e3436";;
+  if (isWhite(key)) {
+    // console.log("it was white");
+    setTimeout(function(){clearAnimation(key,"white");}, 2000);
+  } else { //it is black
+    setTimeout(function(){clearAnimation(key,"black");}, 2000);
+  }
 };
+
+function clearAnimation(keyPressed, color) {
+  keyDOMRef = document.getElementById("key"+keyPressed);
+  keyDOMRef.style.background = color;
+  keyDOMRef.innerHTML = "";
+}
+
 }
